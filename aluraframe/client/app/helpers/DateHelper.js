0ae246a461a8a324a4af7d76deab6c93c03bc1e7
Lucas Figueiredo <lucas-fj@hotmail.com>
2019-11-14 01:15:51 +0100

    Aula 05 - Temos o modelo, mas e a view?

--- aluraframe/client/app/helpers/DateHelper.js
+++ aluraframe/client/app/helpers/DateHelper.js
@@ -5,13 +5,13 @@ class DateHelper {
     }
 
     static textoParaData(texto) {
-        if (!/\d{4}-\d{2}-\d{2}/.test(texto))
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto))
             throw new Error('Deve estar no formato aaaa-mm-dd');
 
         return new Date(...texto.split('-').map((item, index) => (index == 1) ? item - 1 : item)); 
     }
 
     static dataParaTexto(data) {
-        return `${data.getDate()}/${data.getMonth+1}/${data.getYear()}`;
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getYear()}`;
     }
 }
