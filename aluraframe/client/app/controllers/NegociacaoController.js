bd59b1308dc979048f4ca16951cea6dd5566dd45
Lucas <lucas-fj@hotmail.com>
2020-03-18 19:22:40 +0100

    Aula 10 - Importando negociações

--- aluraframe/client/app/controllers/NegociacaoController.js
+++ aluraframe/client/app/controllers/NegociacaoController.js
@@ -38,4 +38,18 @@ class NegociacaoController {
 
         this._inputData.focus();
     }
+
+    importaNegociacoes(){
+        let service = new NegociacaoService();
+
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if (erro) {
+                this._mensagem.texto = erro;
+                return;
+            }
+
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = "Negociaçoes importadas com sucesso!";
+        });
+    }
 }
